930dbf1938801d4023b8ec405fd97fc23738a5fa
LemonGraaaab <77823417+LemonGraaaab@users.noreply.github.com>
2021-06-16 19:15:33 -0700

    test

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { middleware, errorMiddleware } = require('@envoy/envoy-integrations-sdk');
+
+const app = express();
+
+app.post('/hello-options', (req, res) => {
+  res.send([
+    {
+      label: 'Hello',
+      value: 'Hello',
+    },
+    {
+      label: 'Hola',
+      value: 'Hola',
+    },
+    {
+      label: 'Aloha',
+      value: 'Aloha',
+    },
+  ]);
+});
+
+
+app.post('/entry-sign-in', async (req, res) => {
+  const envoy = req.envoy; // our middleware adds an "envoy" object to req.
+  const job = envoy.job;
+  const hello = envoy.meta.config.HELLO;
+  const visitor = envoy.payload;
+  const visitorName = visitor.attributes['full-name'];
+  
+  const message = `${hello} ${visitorName}!`; // our custom greeting
+  await job.attach({ label: 'Hello', value: message }); // show in the Envoy dashboard.
+  
+  res.send({ hello });
+});
+
+app.use(errorMiddleware());
+
+const listener = app.listen(process.env.PORT || 0, () => {
+  console.log(`Listening on port ${listener.address().port}`);
+});
