5adbb114c5f9a27d06f2bc102d97d3d95b4a4896
LemonGraaaab <77823417+LemonGraaaab@users.noreply.github.com>
2021-06-21 11:44:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ const axios = require('axios')
 
 const app = express();
 
+function refresh() { 
+    console.log("REFRESHHHHHH");
+}
+
+function queryOccupany(tokenStr){
+  // create a promise for the axios request
+    const promise = axios.get("https://jyi8o8b1tb.execute-api.us-west-1.amazonaws.com/prod/api/v2/streams?hive_id=123&data_type=occupancy_raw&start=123&database=eval",{ headers: {"Authorization" : `Bearer ${tokenStr}`} })
+
+    // using .then, create a new promise which extracts the data
+    const dataPromise = promise.then((response) => response.data)
+
+    // return it
+    return dataPromise
+}
+
 app.get('/hello-options', (req, res) => {
   res.send([
     {
@@ -24,11 +39,21 @@ app.get('/hello-options', (req, res) => {
 });
 
 app.get('/test', async (req, res) =>{
-  let tokenStr = 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImRjNGQwMGJjM2NiZWE4YjU0NTMzMWQxZjFjOTZmZDRlNjdjNTFlODkiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiRG9uZyIsInJvbGVzIjpbInN1cGVydXNlciJdLCJjbGllbnRfaWQiOiJjbGllbnRfMXJHanpidE43bXhSVmlaMXJwbUk2WVpvcVo2IiwiaXNzIjoiaHR0cHM6Ly9zZWN1cmV0b2tlbi5nb29nbGUuY29tL2J1dGxyLWNsaWVudCIsImF1ZCI6ImJ1dGxyLWNsaWVudCIsImF1dGhfdGltZSI6MTYyNDA3NTIzOSwidXNlcl9pZCI6InVzZXJfMGQyOGM2M2FhMzU1M2MwNDE2ZTMzNzYzMGI3MzZmODcyOGQ1MmQwYiIsInN1YiI6InVzZXJfMGQyOGM2M2FhMzU1M2MwNDE2ZTMzNzYzMGI3MzZmODcyOGQ1MmQwYiIsImlhdCI6MTYyNDA3NTIzOSwiZXhwIjoxNjI0MDc4ODM5LCJlbWFpbCI6ImRvbmdsb25nMTk5MzEyQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImRvbmdsb25nMTk5MzEyQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.mawIoMaNheVKOJi43-ZssMGrRq7fJo-NBT1UXGA12OcsrXxLjTgthakEHXL60b38sx9eWQer1Lp3qNfOf7ZpAYw0hznunDki5SNh9vJb-YTgE75HTkMDlPC09iunxTSFulXYZ30tlN57w00_2ezTXtdyTVMT0oRtOSV4qXHmZmnK5yjy7ozpBvEzIp4Io6ckcWgM4XazB8Kv0EIp5sBVhc6IyVLZxHzOKxJ7sE00Wwyg3vt09sTA4E9KUEmxRNTtGEYHSAMDmqA_IK16kfgp4SJiDAI_CEil2HZJZiwItT39JfFv9d9DMoSvtFQru3STm7LrFgm0N5M5M-V60JQqVQ';
-
-  axios.get("https://jyi8o8b1tb.execute-api.us-west-1.amazonaws.com/prod/api/v2/streams?hive_id=1234&data_type=occupancy_raw&start=123",{ headers: {"Authorization" : `Bearer ${tokenStr}`} })
-       .then(response => res.json(response.data))
-       .catch(err => res.send(err));
+  let tokenStr = 'eyJhbGciOiJSUzI1NiIsImtpZCI6Ijg4ZGYxMzgwM2I3NDM2NjExYWQ0ODE0NmE4ZGExYjA3MTg2ZmQxZTkiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiRG9uZyIsInJvbGVzIjpbInN1cGVydXNlciJdLCJjbGllbnRfaWQiOiJjbGllbnRfMXJHanpidE43bXhSVmlaMXJwbUk2WVpvcVo2IiwiaXNzIjoiaHR0cHM6Ly9zZWN1cmV0b2tlbi5nb29nbGUuY29tL2J1dGxyLWNsaWVudCIsImF1ZCI6ImJ1dGxyLWNsaWVudCIsImF1dGhfdGltZSI6MTYyNDI0OTAzNiwidXNlcl9pZCI6InVzZXJfMGQyOGM2M2FhMzU1M2MwNDE2ZTMzNzYzMGI3MzZmODcyOGQ1MmQwYiIsInN1YiI6InVzZXJfMGQyOGM2M2FhMzU1M2MwNDE2ZTMzNzYzMGI3MzZmODcyOGQ1MmQwYiIsImlhdCI6MTYyNDI0OTAzNiwiZXhwIjoxNjI0MjUyNjM2LCJlbWFpbCI6ImRvbmdsb25nMTk5MzEyQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImRvbmdsb25nMTk5MzEyQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.kVIrHuCjxCE_gzlBUrYlcHmw02S-7ICnhYGZ0HefcsQWa-K8mKED4eJ39_1e7GfURkgSpdcvYwXUVZfDqH0mSzZ_Y81xm3F_0hUGG0A6VYur7EP_8euLrg0h-7v269Iq-A5WFHtU0a-2NCTiJa_-y8D0NE5egQsOZEQng71iCcnTcyXHzsrZDtwGK0o_-tK-C44cRPDEUVd3obUjxOjNN4MC0MwuARfHVg4Do-vwJRbESYFMJG0-SMc5WsrUPqJEZ7S4WDiMXcL7e5WjrKtJ0Yt7JIyrtDfX1GXiqo73d-vMXux6EbDKDn2mwJXPgRWr08KX18ey-elMT3ITgoCssA.eyJuYW1lIjoiRG9uZyIsInJvbGVzIjpbInN1cGVydXNlciJdLCJjbGllbnRfaWQiOiJjbGllbnRfMXJHanpidE43bXhSVmlaMXJwbUk2WVpvcVo2IiwiaXNzIjoiaHR0cHM6Ly9zZWN1cmV0b2tlbi5nb29nbGUuY29tL2J1dGxyLWNsaWVudCIsImF1ZCI6ImJ1dGxyLWNsaWVudCIsImF1dGhfdGltZSI6MTYyNDA3NTIzOSwidXNlcl9pZCI6InVzZXJfMGQyOGM2M2FhMzU1M2MwNDE2ZTMzNzYzMGI3MzZmODcyOGQ1MmQwYiIsInN1YiI6InVzZXJfMGQyOGM2M2FhMzU1M2MwNDE2ZTMzNzYzMGI3MzZmODcyOGQ1MmQwYiIsImlhdCI6MTYyNDA3NTIzOSwiZXhwIjoxNjI0MDc4ODM5LCJlbWFpbCI6ImRvbmdsb25nMTk5MzEyQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImRvbmdsb25nMTk5MzEyQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.mawIoMaNheVKOJi43-ZssMGrRq7fJo-NBT1UXGA12OcsrXxLjTgthakEHXL60b38sx9eWQer1Lp3qNfOf7ZpAYw0hznunDki5SNh9vJb-YTgE75HTkMDlPC09iunxTSFulXYZ30tlN57w00_2ezTXtdyTVMT0oRtOSV4qXHmZmnK5yjy7ozpBvEzIp4Io6ckcWgM4XazB8Kv0EIp5sBVhc6IyVLZxHzOKxJ7sE00Wwyg3vt09sTA4E9KUEmxRNTtGEYHSAMDmqA_IK16kfgp4SJiDAI_CEil2HZJZiwItT39JfFv9d9DMoSvtFQru3STm7LrFgm0N5M5M-V60JQqVQ';
+  queryOccupany(tokenStr)
+    .then(data => {
+        response.json({ message: 'Occupany Data:', data })
+    })
+    .catch(err => function(err)){
+      if(err.response && err.response.status === 404){
+        refresh()
+      }else{
+        res.send(err.status)
+      }
+    }
+  // axios.get("https://jyi8o8b1tb.execute-api.us-west-1.amazonaws.com/prod/api/v2/streams?hive_id=123&data_type=occupancy_raw&start=123&database=eval",{ headers: {"Authorization" : `Bearer ${tokenStr}`} })
+  //      .then(response => res.json(response.data))
+  //      .catch(err => res.send(err));
 });
 
 app.post('/entry-sign-in', async (req, res) => {
